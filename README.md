e7500ba855d6fff4e55b08ccc461820afde27ae4
Thomas <totolamothe65@gmail.com>
2020-04-06 16:04:59 +0200

    Adding new pages, Modal to fix

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# iOS UI CSS Kit
+# [in development] CSS Library based on iOS 13 design
 
 This is free iOS based UI ressources written in SCSS. 
 It's like Bootstrap but inspired by Apple's design.
@@ -11,4 +11,4 @@ It's like Bootstrap but inspired by Apple's design.
 
 ## Disclaimer
 
-I'm not the designer, the design was made by Apple Corp. I just integrated this to provide a Free UI Ressources.
+I'm not the designer, the design was made by Apple Corp. I just integrated this to provide free UI Ressources.
---
+++ assets/js/app.js
@@ -0,0 +1,33 @@
+$(function(){
+    // Check if you have a bottom defined to prevent z-index that make the bottom of the page invisible
+    if($('.is-bottom-menu').length > 0){
+        $('.is-main-container').css({
+            'padding-bottom': $('.is-bottom-menu').height() + 40
+        });
+    }
+
+    // Profil picture
+    $('.is-profil-picture').each(function(){
+        $(this).css({
+            'background': 'url('+ $(this).attr('data-image') +')',
+            'background-size': 'cover'
+        });
+    });
+
+    // Modal part
+    $('[data-toggle="modal"]').click(function(){
+        var modal = $(this).attr('data-target');
+
+        $('' + modal).css({
+            'display': 'block'
+        });
+    });
+
+
+    // To close a modal
+    $('.is-modal-close').click(function(){
+        $(this).parent('.is-modal-top').css({
+            'display': 'none'
+        });
+    });
+});
