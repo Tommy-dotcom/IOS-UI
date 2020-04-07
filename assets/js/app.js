e32de6246ef0222f9f3e4a92dddc9a9608d0527a
Thomas <totolamothe65@gmail.com>
2020-04-07 12:25:31 +0200

    Fixing some design effects : blur

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,21 +1,21 @@
-$(function(){
+$(function() {
     // Check if you have a bottom defined to prevent z-index that make the bottom of the page invisible
-    if($('.is-bottom-menu').length > 0){
+    if ($('.is-bottom-menu').length > 0) {
         $('.is-main-container').css({
             'padding-bottom': $('.is-bottom-menu').height() + 40
         });
     }
 
     // Profil picture
-    $('.is-profil-picture').each(function(){
+    $('.is-profil-picture').each(function () {
         $(this).css({
-            'background': 'url('+ $(this).attr('data-image') +')',
+            'background': 'url(' + $(this).attr('data-image') + ')',
             'background-size': 'cover'
         });
     });
 
     // Alert part
-    $('[data-toggle="alert"]').click(function(){
+    $('[data-toggle="alert"]').click(function () {
         var alert = $(this).attr('data-target');
 
         $('' + alert).css({
@@ -25,7 +25,7 @@ $(function(){
 
 
     // To close an Alert
-    $('.is-alert-close').click(function(){
+    $('.is-alert-close').click(function () {
         $(this).parent('.is-alert-top').css({
             'display': 'none'
         });
@@ -38,19 +38,11 @@ $(function(){
     });
 
     // Is fullscreen
-    $('.is-fullscreen').each(function(){
+    $('.is-fullscreen').each(function () {
         $(this).css({
             'background': 'url(' + $(this).attr('data-background') + ')',
             'background-size': 'cover'
         });
     });
 
-    // Is Fullscreen Content Blured
-    $('head').append("<style>" +
-        ".is-content-fullscreen-blured:before{" +
-        "   background: url("+ $('.is-content-fullscreen-blured').parent('.is-fullscreen').attr('data-background') +");" +
-        "   background-size: cover;" +
-        "   filter: blur(14px);" +
-        "}" +
-        "</style>");
 });
